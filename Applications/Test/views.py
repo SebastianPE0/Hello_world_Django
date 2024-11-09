6b81bc73d967115abbbc5b8d8585057c8c5b318d
SebastianPE0 <seb.perez02@gmail.com>
2024-11-08 23:24:40 -0500

    feat(New Feature): :sparkles: Add a template called Index.html for the message

--- Applications/Test/views.py
+++ Applications/Test/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def hello_world(request):
-    return HttpResponse("Hello_World")
+def index(request):
+    return render(request,"index.html")
